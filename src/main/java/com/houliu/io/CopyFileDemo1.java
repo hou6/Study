b5fe89756216de550daf10d92b6712bc54ceb584
hou6 <houliu114825@163.com>
2020-01-13 23:10:21 +0800

    IO and Properties

---
+++ src/main/java/com/houliu/io/CopyFileDemo1.java
@@ -0,0 +1,31 @@
+package com.houliu.io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @author houliu
+ * @create 2020-01-13 15:15
+ */
+public class CopyFileDemo1 {
+
+    /*
+        使用字节流读取汉字
+            GBK:一个汉字占两个字节
+            UTF-8：一个汉字占三个字节
+     */
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream("/Users/houliu/IdeaProjects/Study/src/main/java/com/houliu/io/h2.txt");
+        FileOutputStream fos = new FileOutputStream("/Users/houliu/IdeaProjects/Study/src/main/java/com/houliu/io/h3.txt");
+        int len = 0;  //每次读取的有效字节个数
+        byte[] bytes = new byte[1024];
+        while ((len = fis.read(bytes)) != -1){
+            fos.write(bytes,0,len);
+        }
+        fos.close();
+        fis.close();
+    }
+
+}
